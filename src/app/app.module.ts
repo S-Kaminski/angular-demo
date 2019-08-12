326b33ed453834eda42720eb258e9f70ffe9ac3d
Simon Kaminski <szy.kaminski@gmail.com>
2019-08-12 19:37:47 +0200

    Fixed a few things realted to the same components' name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ExampleComponent } from './components/example.component';
+import { ExamplemanuallyComponent } from './components/exampleManually.component';
+import { ExampleComponent } from './components/cliExample/example/example.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    ExamplemanuallyComponent,
     ExampleComponent
   ],
   imports: [
