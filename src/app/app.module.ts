16446aa3a3a0e83691c456c3d6b10f5c8f8d121a
Simon Kaminski <szy.kaminski@gmail.com>
2019-08-12 20:03:27 +0200

    New: Example2 generated by CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ExampleComponent } from './components/example.component';
+import { Example2Component } from './components/exampleCLI/example2/example2.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent
+    ExampleComponent,
+    Example2Component
   ],
   imports: [
     BrowserModule
