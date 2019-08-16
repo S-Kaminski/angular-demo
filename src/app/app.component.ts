5f368c1abe1a9dd05745263b1f221fba3facb16f
Simon Kaminski <szy.kaminski@gmail.com>
2019-08-16 17:01:45 +0200

    Modified: Removed redundant code, added Routed and declarations of new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,49 +7,5 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-demo';
-  name = 'Welcome fellow GitHub code-peeker';
-  author = 'S-Kaminski';
-  inputText: string;
-  status: boolean;
-  posts: object[];
-  user = {
-    username: '',
-    password: ''
-  };
-
-  constructor(){
-    console.log('[TEST]Constructor into log');
-    this.getUserStatus();
-    this.posts = [
-      { title: 'Post1' },
-      { title: 'Post2' },
-      { title: 'Post3' },
-      { title: 'Post4' },
-      { title: 'Post5' }
-    ];
-  }
-  displayAuthor(){
-    return this.author;
-  }
-  getUserStatus(){
-    this.status = true;
-  }
-  buttonClicked(){
-    alert('Welcome' + name);
-  }
-  mouseOverBox(){
-    console.log('Mouse is over the black box!');
-  }
-  keyDown(){
-    console.log('Key down!');
-  }
-  onSubmit(form1: NgForm){
-    alert('Submitted');
-    this.user.username = form1.value.username;
-    this.user.password = form1.value.password
-    console.log('Username: ' + this.user.username);
-    console.log('Password: ' + this. user.password);
-    console.log(this.user);
-  }
+  
 }
